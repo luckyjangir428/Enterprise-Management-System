a779f54046f8edc59d06d8adb16bebd486d5fb94
luckyjangir428 <luckyjangid6867@gmail.com>
2026-09-17 01:47:17 +0530

    feat: configure PostgreSQL database connection

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const pool = require("./config/db");
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.json({
-    message: "git statusBackend is running",
+    message: "Backend is running",
   });
+});
+
+app.get("/db-test", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json({
+      message: "Database connected successfully",
+      time: result.rows[0].now,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Database connection failed",
+    });
+  }
 });
 
 app.listen(PORT, () => {
